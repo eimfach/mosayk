bd4a64a10cba865f03335fa1db3bfb38e628780b
eimfach <eimfach@me.com>
2018-07-10 18:44:05 +0200

    V1

---
+++ index.js
@@ -0,0 +1,30 @@
+const expose = Object.freeze({
+  sequentialIterator,
+  timeout
+})
+
+module.exports = expose
+
+function sequentialIterator (arr, sequence) {
+  arr[Symbol.iterator] = function * () {
+    while (arr.length) {
+      if (arr.length < sequence) {
+        yield arr.splice(0, arr.length)
+      } else {
+        yield arr.splice(0, sequence)
+      }
+    }
+  }
+
+  return arr[Symbol.iterator]()
+}
+
+function timeout (length = 0) {
+  const timeout = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve()
+    }, length)
+  })
+
+  return timeout
+}
---
+++ test/sequentialIterator.js
@@ -0,0 +1,32 @@
+const test = require('tape')
+const mosayk = require('../index')
+
+test('sequentialIterator() returns an iterable', (assert) => {
+  const expected =
+    typeof createIterableArray([])[Symbol.iterator]
+
+  const actual =
+    typeof mosayk.sequentialIterator([], 2)[Symbol.iterator]
+
+  assert.equal(actual, expected)
+  assert.end()
+})
+
+test('sequentialIterator() returns sequences', (assert) => {
+  const expected = [[1, 2], [3, 4], [5, 6], [7, 8], [9]]
+
+  const actual = []
+  const iterable = mosayk.sequentialIterator([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)
+
+  for (let seq of iterable) {
+    actual.push(seq)
+  }
+
+  assert.deepEqual(actual, expected)
+  assert.end()
+})
+
+function createIterableArray (arr) {
+  arr[Symbol.iterator] = function * () {}
+  return arr[Symbol.iterator]()
+}
